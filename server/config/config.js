7f0534d4fafdcdbbdd1289c21d6719a0577929c3
plfarfanc85 <plfarfanc@gmail.com>
2021-01-19 15:16:10 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,6 @@ let urlDB;
 //if (process.env.NODE_ENV === "dev") {
 //  urlDB = "mongodb://127.0.0.1:27017/cafe";
 //} else {
-urlDB =
-  "mongodb+srv://cafe-user:r44FWP0Q4cNilDgE@cluster0.j4xjz.mongodb.net/cafe?retryWrites=true&w=majority";
+urlDB = process.env.MONGO_URI;
 //}
 process.env.URLDB = urlDB;
